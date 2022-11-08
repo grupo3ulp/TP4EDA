c0add5e20751a834a9c8dedcb4c911bb28d5464f
iwentothewoods <112993603+iwentothewoods@users.noreply.github.com>
2022-11-08 14:21:57 -0300

    Update Ejercicio 2

--- Guia4ej2.cpp
+++ Guia4ej2.cpp
@@ -1,33 +1,44 @@
+/*Escriba una función que reciba un vector y suma sus elementos pares, reutilizar las funciones
+del punto 1 para cargar y mostrar los vectores*/
+
 #include <iostream>
 using namespace std;
 
-//Ejercicio 2: Escriba una función que reciba un vector y suma sus elementos pares, reutilizar las funciones
-//del punto 1 para cargar y mostrar los vectores
-
-int vector (int lista[]){
-    for (int i = 0; i < 6; ++i) {
-        cin>>lista[i];
+void llenarVector(int vector[], int n){
+    cout << "A continuación ingrese los números que desea guardar en su vector" << endl;
+    for (size_t i = 0; i < n; i++)    {
+        cout << "Posición [" << i << "]" <<endl;
+        cin >> vector[i];
     }
 }
 
-int mostrarPares (int lista[]){
-    int sum = 0;
-    for (int i = 0; i < 6; ++i) {
-        cout<<lista[i]<<" ";
-        if(lista[i]%2==0){
-            sum = sum + lista[i];
-        }
+void mostrarVector(int vector[], int n){
+    for (size_t i = 0; i < n; i++)    {
+        cout << "Posición [" << i << "]: " << vector[i] << endl;
     }
-    cout<<endl;
-    cout<<"La suma de los numeros pares del vector es:"<<endl;
-    cout<<sum<<endl;
+    
 }
 
-int main() {
-    int lista[6];
-    cout<<"Ingrese 6 numeros a la lista"<<endl;
-    vector(lista);
-    cout<<"Se muestra el vector:"<<endl;
-    mostrarPares(lista);
+int sumaVector(int vector[], int n){
+int suma = 0;
+    for (size_t i = 0; i < n; i++)    {
+        if (vector[i]%2 == 0){
+            suma = suma + vector[i];
+        }      
+    }
+return suma;
+}
+
+int main(){
+    int n;
+    cout << "¿De qué tamaño quiere que sea su vector?" << endl;
+    cin >> n;
+
+    int vector[n];
+
+    llenarVector(vector, n);
+    mostrarVector(vector, n);
+    cout << "La suma de los números pares dentro del vector es igual a: " << sumaVector(vector, n) << endl;
+
     return 0;
 }
