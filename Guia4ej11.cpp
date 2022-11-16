cdc53209fa07945fd6913a07d3a5a514574718d4
Ni-luteno <110200618+Ni-luteno@users.noreply.github.com>
2022-11-16 19:11:35 -0300

    Simplifico el metodo
    Es el mismo metodo realmente pero simplificado.

--- Guia4ej11.cpp
+++ Guia4ej11.cpp
@@ -5,23 +5,14 @@ using namespace std;
 //11. Haga un programa en C++ que devuelva la parte fraccionaria de cualquier número introducido
 //por el usuario. Ejemplo, si introducís el número 256.879, debería desplegarse el número 0.879
 
-float fraccionaria (float num){
-    float frac;
-    if(num<0){
-        frac = num - floorf(num);
-    }else if(num>0){
-        frac = num - floorf(num);
-    }else{
-        frac = 0;
-    }
-    return frac;
+float decimales(float n) {
+    return n - floorf(n);
 }
 
 int main() {
     float num;
     cout<<"Ingrese un numero"<<endl;
     cin>>num;
-    cout<<"La fraccinaria de "<<num<<" es:"<<endl;
-    cout<<fraccionaria(num)<<endl;
+    cout<<"La fraccionaria de "<<num<<" es:"<<decimales(num)<<endl;
     return 0;
 }
